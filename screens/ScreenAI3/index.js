2d859b2e26c4356d8777e076c62d7a530e236b86
Guilherme Livramento <guilherme.livramento.fsl@crowdbotics.com>
2023-05-08 15:34:28 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI3/index.js
+++ screens/ScreenAI3/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 const PetProfileScreen = () => {
