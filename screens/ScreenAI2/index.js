f9e998f052e654b7e380d8bebef0d8557991ae37
Guilherme Livramento <guilherme.livramento.fsl@crowdbotics.com>
2023-05-09 11:53:25 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI2/index.js
+++ screens/ScreenAI2/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, Pressable } from "react-native";
 
