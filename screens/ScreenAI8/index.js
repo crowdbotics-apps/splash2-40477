ee037b3021a9e033652ea59de258d0407d7a2961
Guilherme Livramento <guilherme.livramento.fsl@crowdbotics.com>
2023-05-12 10:19:44 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI8/index.js
+++ screens/ScreenAI8/index.js
@@ -1,33 +1,33 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput, Picker } from 'react-native';
-import pet1 from '../assets/pet1.png';
-import pet2 from '../assets/pet2.png';
-import pet3 from '../assets/pet3.png';
-import pet4 from '../assets/pet4.png';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput, Picker } from "react-native";
+import pet1 from "../assets/pet1.png";
+import pet2 from "../assets/pet2.png";
+import pet3 from "../assets/pet3.png";
+import pet4 from "../assets/pet4.png";
 
 const SnackTimeScreen = ({
   navigation
 }) => {
   const [selectedPet, setSelectedPet] = useState(null);
   const [treats, setTreats] = useState([]);
-  const [brands, setBrands] = useState('');
+  const [brands, setBrands] = useState("");
   const [food, setFood] = useState([]);
-  const [foodBrands, setFoodBrands] = useState('');
+  const [foodBrands, setFoodBrands] = useState("");
   const pets = [{
     id: 1,
-    name: 'Buddy',
+    name: "Buddy",
     image: pet1
   }, {
     id: 2,
-    name: 'Luna',
+    name: "Luna",
     image: pet2
   }, {
     id: 3,
-    name: 'Max',
+    name: "Max",
     image: pet3
   }, {
     id: 4,
-    name: 'Charlie',
+    name: "Charlie",
     image: pet4
   }];
 
@@ -44,7 +44,7 @@ const SnackTimeScreen = ({
 
   return <View style={styles.container}>
       <View style={styles.header}>
-        <Image source={require('../assets/pettopia-logo.png')} style={styles.logo} />
+        <Image source={require("../assets/pettopia-logo.png")} style={styles.logo} />
       </View>
       <View style={styles.petSelection}>
         {pets.map(pet => <TouchableOpacity key={pet.id} style={[styles.petItem, selectedPet?.id === pet.id && styles.selectedPetItem]} onPress={() => handlePetSelection(pet)}>
@@ -57,7 +57,9 @@ const SnackTimeScreen = ({
       </View>
       <View style={styles.form}>
         <View style={styles.formField}>
-          <Text style={styles.label}>What type of treats does {selectedPet?.name} like?*</Text>
+          <Text style={styles.label}>
+            What type of treats does {selectedPet?.name} like?*
+          </Text>
           <Picker selectedValue={treats} onValueChange={value => setTreats(value)} mode="dropdown" prompt="Select" style={styles.picker} itemStyle={styles.pickerItem} multiple>
             <Picker.Item label="Option 1" value="option1" />
             <Picker.Item label="Option 2" value="option2" />
@@ -69,7 +71,9 @@ const SnackTimeScreen = ({
           <TextInput style={styles.input} placeholder="Brand name" value={brands} onChangeText={text => setBrands(text)} />
         </View>
         <View style={styles.formField}>
-          <Text style={styles.label}>What type of food does {selectedPet?.name} like? *</Text>
+          <Text style={styles.label}>
+            What type of food does {selectedPet?.name} like? *
+          </Text>
           <Picker selectedValue={food} onValueChange={value => setFood(value)} mode="dropdown" prompt="Select" style={styles.picker} itemStyle={styles.pickerItem} multiple>
             <Picker.Item label="Option 1" value="option1" />
             <Picker.Item label="Option 2" value="option2" />
@@ -82,16 +86,19 @@ const SnackTimeScreen = ({
         </View>
       </View>
       <View style={styles.funFactContainer}>
-        <Text style={styles.funFact}>Fun fact: Don’t worry, you’ll be able to change these details at a later date</Text>
+        <Text style={styles.funFact}>
+          Fun fact: Don’t worry, you’ll be able to change these details at a
+          later date
+        </Text>
       </View>
       <View style={styles.buttonsContainer}>
         <TouchableOpacity style={styles.backButton} onPress={handleBack}>
           <Text style={styles.buttonText}>Back</Text>
-          <Image source={require('../assets/left-arrow.png')} style={styles.buttonIcon} />
+          <Image source={require("../assets/left-arrow.png")} style={styles.buttonIcon} />
         </TouchableOpacity>
         <TouchableOpacity style={styles.nextButton} onPress={handleNext}>
           <Text style={styles.buttonText}>Next</Text>
-          <Image source={require('../assets/right-arrow.png')} style={styles.buttonIcon} />
+          <Image source={require("../assets/right-arrow.png")} style={styles.buttonIcon} />
         </TouchableOpacity>
       </View>
       <View style={styles.footer}>
@@ -103,22 +110,22 @@ const SnackTimeScreen = ({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff"
   },
   header: {
-    backgroundColor: '#22B9B0',
+    backgroundColor: "#22B9B0",
     height: 80,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   logo: {
     width: 120,
     height: 40
   },
   petSelection: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
     marginVertical: 20
   },
   petItem: {
@@ -126,12 +133,12 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 25,
     borderWidth: 2,
-    borderColor: '#ccc',
-    justifyContent: 'center',
-    alignItems: 'center'
+    borderColor: "#ccc",
+    justifyContent: "center",
+    alignItems: "center"
   },
   selectedPetItem: {
-    borderColor: '#22B9B0'
+    borderColor: "#22B9B0"
   },
   petImage: {
     width: 40,
@@ -139,17 +146,17 @@ const styles = StyleSheet.create({
     borderRadius: 20
   },
   titleContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginVertical: 20
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333'
+    fontWeight: "bold",
+    color: "#333"
   },
   subtitle: {
     fontSize: 16,
-    color: '#666'
+    color: "#666"
   },
   form: {
     paddingHorizontal: 20
@@ -159,12 +166,12 @@ const styles = StyleSheet.create({
   },
   label: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5
   },
   picker: {
     height: 50,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
     borderRadius: 5,
     paddingHorizontal: 10
   },
@@ -173,42 +180,42 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 50,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
     borderRadius: 5,
     paddingHorizontal: 10
   },
   funFactContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginVertical: 20
   },
   funFact: {
     fontSize: 14,
-    color: '#666'
+    color: "#666"
   },
   buttonsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     paddingHorizontal: 20,
     marginBottom: 20
   },
   backButton: {
-    backgroundColor: '#22B9B0',
-    flexDirection: 'row',
-    alignItems: 'center',
+    backgroundColor: "#22B9B0",
+    flexDirection: "row",
+    alignItems: "center",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5
   },
   nextButton: {
-    backgroundColor: '#22B9B0',
-    flexDirection: 'row',
-    alignItems: 'center',
+    backgroundColor: "#22B9B0",
+    flexDirection: "row",
+    alignItems: "center",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
     marginRight: 10
   },
@@ -217,15 +224,15 @@ const styles = StyleSheet.create({
     height: 20
   },
   footer: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-end",
     flex: 1
   },
   line: {
     height: 1,
-    width: '100%',
-    backgroundColor: '#ccc'
+    width: "100%",
+    backgroundColor: "#ccc"
   }
 });
 export default SnackTimeScreen;
